50970c6f0cae3cf93f207835255cc27e7e4ffeef
AMYunus <ahmad.mahmudi.yunus@gmail.com>
2020-01-26 10:41:41 +0700

    Chapter 1 Creating and Combining Views

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by AMYunus on 26/01/20.
+//  Copyright © 2020 amyunus. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center:coordinate, span:span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
